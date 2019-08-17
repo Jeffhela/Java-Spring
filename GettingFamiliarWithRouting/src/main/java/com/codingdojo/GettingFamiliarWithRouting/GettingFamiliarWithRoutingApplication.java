dfc98bec821aab9c61c3327faad5abb1afef9d17
Jeffrey <jeromebro@msn.com>
2019-08-16 21:33:58 -0500

    Add files via upload

---
+++ GettingFamiliarWithRouting/src/main/java/com/codingdojo/GettingFamiliarWithRouting/GettingFamiliarWithRoutingApplication.java
@@ -0,0 +1,14 @@
+package com.codingdojo.GettingFamiliarWithRouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GettingFamiliarWithRoutingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GettingFamiliarWithRoutingApplication.class, args);
+	}
+
+}
